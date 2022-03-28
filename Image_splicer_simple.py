4b3204c7b35543d895f7d9534072b70ab84b9433
capstone <capstone@public-66-174.emich.edu>
2022-03-28 11:15:43 -0400

    not stitiching images correctly

---
+++ Image_splicer_simple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 11:25:15 2022
+
+@author: capstone
+"""
+
+import cv2
+stitcher = cv2.Stitcher_create()
+img1 = cv2.imread('data/north_c1.png') 
+img2 = cv2.imread('data/north_c2.png') 
+result = stitcher.stitch(img1, img2)
+
+
+cv2.imwrite('plot.png', result[1])
+ 
+
+
+
